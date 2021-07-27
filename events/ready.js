a205370b5faaadd2a55b1f4bb65a3d722e3f3f26
JanVGX <83541534+JanVGX@users.noreply.github.com>
2021-07-28 00:53:39 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
-client.user.setActivity(">help", { type: "STREAMING", url:"https://www.twitch.tv/captain_motchy" }).then(() => {
+client.user.setActivity("g!help | Made by JanVGX", { type: "PLAYING", url:"https://www.twitch.tv/janvgx" }).then(() => {
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag);
   });
